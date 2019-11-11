57ceafefbe7606ca6909c26b78b0fe38405161c6
Pieter Van den Abeele <pvdabeel@mac.com>
2019-11-11 15:32:45 +0100

    Updated rules. Added notion of 'system' profile

--- Source/ebuild.pl
+++ Source/ebuild.pl
@@ -4,13 +4,13 @@
 
 % Case 1: The ebuild has Metadata for the requested key
 
-ebuild:get(Key,Entry,Content) :-
-  cache:entry(_,Entry,_,_,_,_,Metadata),
+ebuild:get(Key,Context://Entry,Content) :-
+  cache:entry(Context,Entry,_,_,_,_,Metadata),
   eapi:elem(Key,Metadata,Content).
 
 
 % Case 2: The ebuild does not have Metadata for the requested key
 
-ebuild:get(Key,Entry,[]) :-
-  cache:entry(_,Entry,_,_,_,_,Metadata),
+ebuild:get(Key,Context://Entry,[]) :-
+  cache:entry(Context,Entry,_,_,_,_,Metadata),
   not(eapi:elem(Key,Metadata,_)).
--- Source/rules.pl
+++ Source/rules.pl
@@ -2,30 +2,103 @@
 % RULES declarations
 % ******************
 
-% Work in progress
-
-% Transition rules...
 
 % An ebuild can be installed, if its compiletime dependencies are satisfied 
 
-rule(_Context://Ebuild:install,Deps) :- 
-  ebuild:get(depend,Ebuild,Deps).
+rule(Context://Ebuild:install,Deps) :- 
+  ebuild:get(depend,Context://Ebuild,Deps).
 
-% An ebuild can be run, if its runtime dependencies are satisfied 
+% An ebuild can be run, if it is installed and if its runtime dependencies are satisfied  
 
-rule(Ebuild:run,Deps) :-
-  ebuild:get(rdepend,Ebuild,Deps).
+rule(Context://Ebuild:run,[Context://Ebuild:install|Deps]) :-
+  ebuild:get(rdepend,Context://Ebuild,Deps).
 
 
-% Conflicting package: EAPI 8.2.6.2: a weak block can be ignored by the package manager
+
+% Ignored for now: Conflicting package: EAPI 8.2.6.2: a weak block can be ignored by the package manager
+
 rule(package_dependency(weak,_,_,_,_,_,_),[]) :- !.
 
-% Conflicting package: EAPI 8.2.6.2: a strong block is satisfied when no suitable candidate is satisfied
+% Ignored for now: Conflicting package: EAPI 8.2.6.2: a strong block is satisfied when no suitable candidate is satisfied
+
 rule(package_dependency(strong,_,_,_,_,_,_),[]) :- !.
 
+% Ignored for now: Virtual package dependencies 
 
-% DEBUG: for now skip virtual dependencies
 rule(package_dependency(_,'virtual',_,_,_,_,_),[]) :- !.
+
+
+
+% Dependencies on the system profile are assumed satisfied
+
+rule(package_dependency(no,'sys-apps','baselayout',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'app-arch','bzip2',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'app-arch','gzip',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'app-arch','tar',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'app-arch','xz-utils',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'app-shells','bash',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'net-misc','iputils',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'net-misc','rsync',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'net-misc','wget',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','coreutils',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','diffutils',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','file',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','findutils',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','gawk',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','grep',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','kbd',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','less',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-process','procps',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-process','psmisc',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-apps','sed',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-devel','binutils',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-devel','gcc',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-devel','gnuconfig',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-devel','make',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-devel','patch',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'sys-fs','e2fsprogs',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','dev-manager',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','editor',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','libc',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','man',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','modutils',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','os-headers',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','package-manager',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','pager',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','service-manager',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','shadow',_,_,_,_),[]) :- !.
+rule(package_dependency(no,'virtual','ssh',_,_,_,_),[]) :- !.
+
+
+% A package dependency is satisfied when a suitable candidate is satisfied
+rule(package_dependency(no,C,N,_,_,_,_),portage://Choice:install) :-
+  cache:entry(_,Choice,_,C,N,_,_). % write(Choice),nl. 
+
+
+
+% Ignored for now: The dependencies in a use conditional group need not to be satisfied when the use flag is not set
+rule(use_conditional_group(_,_,_),[]) :- !.
+
+
+% Ignored for now: One dependency of an any_of_group should be satisfied
+rule(any_of_group(_Deps),[]) :- !.   
+
+
+% Ignored for now: All dependencies in an all_of_group should be satisfied
+rule(all_of_group(_Deps),[]) :- !. 
+
+
+
+
+
+
+
+
+
+
+
+
+% Archived
 
 
 % DEBUG: what if a package doesn't have a cache entry defined? 
@@ -33,26 +106,8 @@ rule(package_dependency(_,'virtual',_,_,_,_,_),[]) :- !.
 %   not(cache:entry(_,_,_,C,N,_,_)),
 %   fail.
 
-% DEBUG: We start with NO, weak and strong are implemented later
-
-% A package dependency is satisfied when a suitable candidate is satisfied
-rule(package_dependency(no,C,N,_,_,_,_),Choice:install) :-
-  cache:entry(_,Choice,_,C,N,_,_).% write(Choice),nl. %DEBUG: no backtracking over versions
-
-
-
-
 % The dependencies in a use conditional group need to be satisfied when the use flag is set
-%rule(use_conditional_group(_,Use,Deps),Deps) :- 
+% rule(use_conditional_group(_,Use,Deps),Deps) :- 
 %  mypreferences:use(Use),!.
 
-% The dependencies in a use conditional group need not to be satisfied when the use flag is not set
-rule(use_conditional_group(_,_,_),[]) :- !.
-
-% One dependency of an any_of_group should be satisfied
-rule(any_of_group(_Deps),[]) :- !.   % DEBUG: Deps
-  %member(Dep,Deps).
-
-% All dependencies in an all_of_group should be satisfied
-rule(all_of_group(_Deps),[]) :- !. %DEBUG: Deps
 
