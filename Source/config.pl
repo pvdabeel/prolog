65481cb1b5ffc1e3367c86b11bf3c215d85bf71e
Pieter Van den Abeele <pvdabeel@mac.com>
2019-11-11 16:32:41 +0100

    Set use flags to test use conditional dependencies

--- Source/config.pl
+++ Source/config.pl
@@ -4,10 +4,11 @@
 %
 % Settings used throughout the program.
 
+config:number_of_cpus(C) :- current_prolog_flag(cpu_count,C).
 
-%current_prolog_flag(cpu_count,X).
-config:number_of_cpus(36).
 config:installation_dir('/Users/pvdabeel/Desktop/Prolog').
+
 config:verbosity(debug).
+
 config:graph_directory('/Users/pvdabeel/Graph/').
 
