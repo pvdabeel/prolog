46ba708cf6df1489dfe44cf717d30720a1a5acab
Pieter Van den Abeele <pvdabeel@mac.com>
2019-11-11 20:37:52 +0100

    Turn into static class && cleanup documentation

--- Source/builder.pl
+++ Source/builder.pl
@@ -1,11 +1,86 @@
+/*                                                                              
+  Author:   Pieter Van den Abeele                                               
+  E-mail:   pvdabeel@mac.com                                                    
+  Copyright (c) 2005-2019, Pieter Van den Abeele                                
+                                                                                
+  Distributed under the terms of the LICENSE file in the root directory of this 
+  project.                                                                      
+*/                                                                              
+                                                                                
+                                                                                
+/** <module> BUILDER                                                         
+The Builder takes a plan from the Planner and executes it. 
+
+The builder is a static class. 
+*/    
+
+:- module(builder, []).
+
 % ********************
 % BUILDER declarations
 % ********************
 
+:- class.
+
+% public interface
+
+:- dpublic('execute'/1).
+:- dpublic('build'/1).
+:- dpublic('test'/1).
+
+% protected interface
+
+:- dprotected('firststep'/1).
+:- dprotected('nextstep'/1).
+
+
+%! builder:execute(+Plan)                                                                        
+%
+% Given a Plan from the Planner, execute the plan. 
+%                                                                               
+% Example: Invoke builder:execute(+Plan) to build or upgrade your operating 
+% system, kernel, software application, etc... as described in the given Plan.
 
 builder:execute(Plan) :-
   forall(member(E,Plan),
     (write(' -  STEP:  | '),builder:firststep(E))).
+
+
+%! builder:build(+Context://Entry:Action)                                                                        
+%
+% Public predicate
+%
+% For a given Entry in a given context, execute Action. 
+%                                                                               
+% Example: Invoke builder:build(portage://myebuild:install to install the 
+% given ebuild in the portage repository in your live system
+% Portage is an example repository of type 'eapi'
+%
+% Example: Invoke builder:build(linux://torvalds/linux-2.4.2:install to install
+% the given Linux kernel release in your live system
+% Linux is an example repository of type 'make' (see portage-ng.pl main)) 
+%
+% Example: Invoke builder:build(swipl://swi-prolog/swipl-8.1.5:install to 
+% install the given SWI-Prolog release in your live system
+% Swipl is an example respository of type 'cmake' (see portage-ng.pl main)
+
+builder:build(Context://Entry:_Action) :-
+  Context:get_type('eapi'),!.
+  portage:get_location(L),
+  portage:ebuild(Entry,Category,Package,Version),
+  atomic_list_concat(['ebuild ',L,'/',Category,'/',Package,'/',Package,'-',Version,'.ebuild install qmerge clean'],Cmd),
+  shell(Cmd),!.
+
+
+builder:build(Context://_Entry:_Action) :-
+  Context:get_type('cmake'),!,
+  Context:get_location(Local),
+  script:exec(build,[Local]).
+
+
+% Helper predicates
+%
+% Protected predicates 
 
 builder:firststep([]) :-  nl, !.
 
@@ -23,7 +98,6 @@ builder:firststep([rule(Context://E:Action,_)|L]) :-
 
 builder:firststep([rule(_,_)|L]) :-
   builder:firststep(L).
-
 
 builder:nextstep([]) :- nl,!.
 
@@ -44,19 +118,15 @@ builder:nextstep([rule(_,_)|L]) :-
   builder:nextstep(L).
 
 
-builder:build(Context://Entry:_Action) :-
-  Context:get_type('eapi'),!,
-  portage:get_location(L),
-  portage:ebuild(Entry,Category,Package,Version),
-  atomic_list_concat(['ebuild ',L,'/',Category,'/',Package,'/',Package,'-',Version,'.ebuild install qmerge clean'],Cmd),
-  shell(Cmd),!.
-
-
-builder:build(Context://_Entry:_Action) :-
-  Context:get_type('cmake'),!,
-  Context:get_location(Local),
-  script:exec(build,[Local]).
-
+%! builder:test(+Repository)                                                                        
+%                                                                               
+% Public predicate                                                              
+%                                                                               
+% For a given Repository, build all entries it contains.     
+%
+% Example: Invoke builder:test(+Repository) to build everything in a repository. 
+% (E.g. every ebuild in a Gentoo repository, or every release of a given 
+% application)
 
 builder:test(Repository) :-
   system:time(
