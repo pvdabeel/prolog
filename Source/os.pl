e40f784898652432b76bc291e59adab7f4dbc48f
Pieter Van den Abeele <pvdabeel@mac.com>
2019-11-15 21:00:51 +0100

    Documentation cleanup

--- Source/os.pl
+++ Source/os.pl
@@ -1,13 +1,25 @@
+/*                                                                              
+  Author:   Pieter Van den Abeele                                               
+  E-mail:   pvdabeel@mac.com                                                    
+  Copyright (c) 2005-2019, Pieter Van den Abeele                                
+                                                                                
+  Distributed under the terms of the LICENSE file in the root directory of this 
+  project.                                                                      
+*/                                                                              
+                                                                                
+                                                                                
+/** <module> OS                                                        
+This file contains predicates used to interact with the operating system
+Goal is to get the same behaviour across different platform. 
+Eventually this could become a class with different subclasses.
+*/    
+
 % ***************
 % OS declarations
 % ***************
-%
-% This file contains predicates used to interact with the operating system
-% Goal is to get the same behaviour across different platform. 
-% Eventually this could become a class with different subclasses.
 
 
-% os:directory_content(+Directory,-Content)
+%! os:directory_content(+Directory,-Content)
 %
 % For a given directory, returns an alphabetical list containing the 
 % content of the directory. Special contents (like '.' and '..') is 
@@ -19,7 +31,7 @@ os:directory_content(Directory,Content) :-
   member(Content,Contents).  
 
 
-% os:compose_path(+Path,+RelativePath,-NewPath)
+%! os:compose_path(+Path,+RelativePath,-NewPath)
 %
 % Given a path (relative or absolute) and a relative path, composes a 
 % new path by combining both paths and a separator.
@@ -28,7 +40,7 @@ os:compose_path(Path,RelativePath,NewPath) :-
   atomic_list_concat([Path,'/',RelativePath],NewPath).
 
 
-% os:make_repository_dirs(Repository,Directory)
+%! os:make_repository_dirs(Repository,Directory)
 %
 % Given a prolog repository, creates a directory with subdirs 
 % corresponding to the categories within the prolog repository
@@ -40,7 +52,7 @@ os:make_repository_dirs(Repository,Directory) :-
      system:make_directory(Subdir))).
 
 
-% os:update_repository_dirs(Repository,Directory)
+%! os:update_repository_dirs(Repository,Directory)
 %
 % Given a prolog repository, creates a directory with subdirs 
 % corresponding to the categories within the prolog repository
