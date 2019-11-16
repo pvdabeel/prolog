ea6c3b41b1ad42870d5b3b501a4a2463dbca0661
Pieter Van den Abeele <pvdabeel@mac.com>
2019-11-16 13:17:50 +0100

    Documentation cleanup

--- Source/script.pl
+++ Source/script.pl
@@ -1,16 +1,27 @@
+/*                                                                              
+  Author:   Pieter Van den Abeele                                               
+  E-mail:   pvdabeel@mac.com                                                    
+  Copyright (c) 2005-2019, Pieter Van den Abeele                                
+                                                                                
+  Distributed under the terms of the LICENSE file in the root directory of this 
+  project.                                                                      
+*/                                                                              
+                                                                                
+                                                                                
+/** <module> SCRIPTSi
+This file declares a predicate to execute a script inside the scripts directory                                                             
+*/     
+
+:- module(script,[]).
+
 % *******************
 % SCRIPT declarations
 % *******************
 
-% Call scripts inside the scripts directory
 
-:- module(script,[]).
-
-% script
+%! script:exec(+Name,+Args)
 %
 % Call script with arguments
-%
-% public predicate
 
 script:exec(S,Args) :-
   is_list(Args),
