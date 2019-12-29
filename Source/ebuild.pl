3450ba5a622a68801d8ae2062d7e59b421e4e20c
Pieter Van den Abeele <pvdabeel@mac.com>
2019-12-29 17:27:25 +0100

    Speed up use flag processing

--- Source/ebuild.pl
+++ Source/ebuild.pl
@@ -115,7 +115,7 @@ ebuild:download(Repository://Entry,B,S) :-
   number_string(S,Ss).
 
 
-%! ebuild:download_size(+Repository://+Entry,-T) 
+%! ebuild:download_size(+Repository://+Entry,-T)
 %
 % Retrieve total download size for all files corresponding to a given Entry
 
@@ -129,38 +129,40 @@ ebuild:iuse(Repository://Entry,Use) :-
   ebuild:get(iuse,Repository://Entry,Iuse),
   member(Use,Iuse).
 
+ebuild:iuse_filtered(Repository://Entry,Use) :-
+  ebuikd:get(iuse_filtered,Repository://Entry,Iuse),
+  member(Use,Iuse).
 
-ebuild:use(Repository://Entry,Use,'pos:preference') :- 
-  ebuild:iuse(Repository://Entry,Use),
-  preference:positive_use(Use).
 
-ebuild:use(Repository://Entry,Use,'neg:preference') :-
-  ebuild:iuse(Repository://Entry,Use),
-  preference:negative_use(Use).
-
-ebuild:use(Repository://Entry,Use,'pos:ebuild') :-
-  ebuild:iuse(Repository://Entry,plus(Use)),
+ebuild:categorize_use(plus(Use),'pos:ebuild') :-
+  !,
   not(preference:positive_use(Use)),
   not(preference:negative_use(Use)).
 
-ebuild:use(Repository://Entry,Use,'neg:ebuild') :-
- ebuild:iuse(Repository://Entry,minus(Use)),
- not(preference:positive_use(Use)),
- not(preference:negative_use(Use)).
+ebuild:categorize_use(minus(Use),'neg:ebuild') :-
+  !,
+  not(preference:positive_use(Use)),
+  not(preference:negative_use(Use)).
 
-ebuild:use(Repository://Entry,Use,'neg:default') :-
- ebuild:iuse(Repository://Entry,Use),
- not(preference:positive_use(Use)),
- not(preference:negative_use(Use)),
- not(printer:unify(plus(_),Use)),
- not(printer:unify(minus(_),Use)).
+ebuild:categorize_use(Use,'pos:preference') :-
+  preference:positive_use(Use),!.
+
+ebuild:categorize_use(Use,'neg:preference') :-
+  preference:negative_use(Use),!.
+
+ebuild:categorize_use(_,'neg:default') :-
+  %not(preference:positive_use(Use)),
+  %not(preference:negative_use(Use)),
+  %not(printer:unify(plus(_),Use)),
+  %not(printer:unify(minus(_),Use)).
+  !.
 
 
 %! ebuild:is_virtual(+Repository://+Entry)
 %
-% True if an entry is a virtual 
+% True if an entry is a virtual
 
-ebuild:is_virtual(Repository://Entry) :- 
+ebuild:is_virtual(Repository://Entry) :-
   Repository:ebuild(Entry,'virtual',_,_).
 
 
