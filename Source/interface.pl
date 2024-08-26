b57377417d0151c1848e5123b76602ddc4133ef5
Pieter Van den Abeele <pvdabeel@mac.com>
2024-08-26 23:07:57 +0200

    Use portage interface

--- Source/interface.pl
+++ Source/interface.pl
@@ -39,26 +39,32 @@ interface:status(S) :-
 % Retrieve the interface specification
 
 interface:spec(S) :-
-  S = [[opt(mode),    type(atom),    default('server'),                    longflags(['mode'] ),
+  S = [[opt(mode),     type(atom),    default('server'),                    longflags(['mode'] ),
         help([ '  server:     start as server'
              , '  standalone: start standalone client, not requireing running server'
              , '  client:     start lightweight client, requiring running server'])],
-       [opt(verbose), type(boolean), default(false),    shortflags(['v']), longflags(['verbose']),  help('Turn on verbose mode')],                       % OPTION
-       [opt(pretend), type(boolean), default(false),    shortflags(['p']), longflags(['pretend']),  help('Turn on pretend mode')],                       % OPTION
-       [opt(merge),   type(boolean), default(true),     shortflags(['m']), longflags(['merge']),    help('Merge target package')],                       % OPTION
-       [opt(update),  type(boolean), default(false),    shortflags(['u']), longflags(['update']),   help('Update target package')],                      % OPTION
-       [opt(deep),    type(boolean), default(false),    shortflags(['d']), longflags(['deep']),     help('Also consider dependencies')],                 % OPTION
-       [opt(resume),  type(boolean), default(false),    shortflags(['r']), longflags(['resume']),   help('Resume previous command')],                    % OPTION
-       [opt(newuse),  type(boolean), default(false),    shortflags(['n']), longflags(['newuse']),   help('Take into account new use flags')],            % OPTION
-       [opt(sync),    type(boolean), default(false),                       longflags(['sync']),     help('Sync repository')],                            % ACTION
-       [opt(clear),   type(boolean), default(false),                       longflags(['clear']),    help('Clear knowledge base')],                       % ACTION
-       [opt(graph),   type(boolean), default(false),                       longflags(['graph']),    help('Create graph')],                               % ACTION
-       [opt(depclean),type(boolean), default(false),    shortflags(['c']), longflags(['depclean']), help('Clean dependencies')],                         % ACTION
-       [opt(info),    type(boolean), default(false),                    longflags(['info']),     help('Show package version')],                          % ACTION
-       [opt(search),  type(boolean), default(false),    shortflags(['s']), longflags(['search']),   help('Search for a target')],                        % ACTION
-       [opt(unmerge), type(boolean), default(false),    shortflags(['C']), longflags(['unmerge']),  help('Unmerge target')],                             % ACTION
-       [opt(shell),   type(boolean), default(false),                    longflags(['shell']),    help('Go to shell')],                                   % ACTION
-       [opt(version), type(boolean), default(false),    shortflags(['V']), longflags(['version']),  help('Show version')]                                % ACTION
+       [opt(verbose),  type(boolean), default(false),    shortflags(['v']), longflags(['verbose']),   help('Turn on verbose mode')],
+       [opt(pretend),  type(boolean), default(false),    shortflags(['p']), longflags(['pretend']),   help('Turn on pretend mode')],
+       [opt(merge),    type(boolean), default(true),     shortflags(['m']), longflags(['merge']),     help('Merge target package')],
+       [opt(update),   type(boolean), default(false),    shortflags(['u']), longflags(['update']),    help('Update target package')],
+       [opt(deep),     type(boolean), default(false),    shortflags(['d']), longflags(['deep']),      help('Also consider dependencies')],
+       [opt(emptytree),type(boolean), default(false),    shortflags(['e']), longflags(['emptytree']), help('Pretend no other packages are installed')],
+       [opt(buildpkg), type(boolean), default(false),    shortflags(['b']), longflags(['buildpkg']),  help('Build packages')],
+       [opt(resume),   type(boolean), default(false),    shortflags(['r']), longflags(['resume']),    help('Resume previous command')],
+       [opt(newuse),   type(boolean), default(false),    shortflags(['N']), longflags(['newuse']),    help('Take into account new use flags')],
+       [opt(oneshot),  type(boolean), default(false),    shortflags(['1']), longflags(['oneshot']),   help('Do not add package to world')],
+       [opt(prefix),   type(atom),    default(false),                       longflags(['prefix']),    help('Set the prefix directory')],
+       [opt(sync),     type(boolean), default(false),                       longflags(['sync']),      help('Sync repository')],
+       [opt(clear),    type(boolean), default(false),                       longflags(['clear']),     help('Clear knowledge base')],
+       [opt(graph),    type(boolean), default(false),                       longflags(['graph']),     help('Create graph')],
+       [opt(depclean), type(boolean), default(false),    shortflags(['c']), longflags(['depclean']),  help('Clean dependencies')],
+       [opt(info),     type(boolean), default(false),                       longflags(['info']),      help('Show package version')],
+       [opt(search),   type(boolean), default(false),    shortflags(['s']), longflags(['search']),    help('Search for a target')],
+       [opt(unmerge),  type(boolean), default(false),    shortflags(['C']), longflags(['unmerge']),   help('Unmerge target')],
+       [opt(usepkg),   type(boolean), default(false),    shortflags(['k']), longflags(['usepkg']),    help('Use prebuilt packages')],
+       [opt(quiet),    type(boolean), default(false),    shortflags(['q']), longflags(['quiet']),     help('Reduced output')],
+       [opt(shell),    type(boolean), default(false),                       longflags(['shell']),     help('Go to shell')],
+       [opt(version),  type(boolean), default(false),    shortflags(['V']), longflags(['version']),   help('Show version')]
       ].
 
 
