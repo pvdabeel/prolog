cf3e4f704c6dba38aa0df037ed6782e96663d27f
Pieter Van den Abeele <pvdabeel@mac.com>
2025-06-07 00:50:40 +0200

    Parser converts strings directly into codes, avoiding atoms.

--- Source/parser.pl
+++ Source/parser.pl
@@ -12,41 +12,41 @@
 The parser takes its input from the reader and applies an EAPI-7 compliant DCG
 grammar to output a parse tree.
 
-Input:  A nested list of character codes codes.
-        Each sublist represents a line.
+Input:  A list of strings.
+        Each string represents a line.
         Each line in a repository cache entry represents a 'key=value' pair.
         Each line in a manifest file represents a 'key value' pair.
 
-Output: A nested list representing the the result of parsing each line in the
-        input.
+Output: A nested list representing the result of parsing each line in the input.
 */
 
-:- module(parser,[]).
+:- module(parser, []).
 
 % *******************
 % PARSER declarations
 % *******************
 
 
-%! parser:invoke(+Type,+Repository://+Entry,+Contents,-Result)
+%! parser:invoke(+Type, +Repository://+Entry, +Contents, -Result)
 %
-% Type:     An atom indicated Contents is of type 'metadata' or 'manifest'.
+% Type:     An atom indicating Contents is of type 'metadata' or 'manifest'.
 %
-% Contents: A nested list of character codes.
+% Contents: A list of strings.
 %
-% Result:   A nested list represnting the parse tree.
+% Result:   A nested list representing the parse tree.
 
-parser:invoke(_,_,[],[]).
+parser:invoke(_, _, [], []).
 
-parser:invoke(Type,Repository://Entry,[Line|Lines],[KeyValue|KeyValues]) :-
-  phrase(eapi:keyvalue(Type,Repository://Entry,KeyValue),Line),!,
-  parser:invoke(Type,Repository://Entry,Lines,KeyValues).
+parser:invoke(Type, Repository://Entry, [Line|Lines], [KeyValue|KeyValues]) :-
+  string_codes(Line, Codes),
+  phrase(eapi:keyvalue(Type, Repository://Entry, KeyValue), Codes), !,
+  parser:invoke(Type, Repository://Entry, Lines, KeyValues).
 
 
 %! parser:test(+Repository)
 %
 % Predicate tests whether all cache entries belonging to a given repository
-% instance can be succesfully parsed.
+% instance can be successfully parsed.
 %
 % Repository: The repository instance from which to parse all entries.
 %
@@ -54,17 +54,17 @@ parser:invoke(Type,Repository://Entry,[Line|Lines],[KeyValue|KeyValues]) :-
 
 parser:test(Repository) :-
   config:test_style(Style),
-  parser:test(Repository,Style).
+  parser:test(Repository, Style).
 
 
-%! parser:test(+Repository,+Style)
+%! parser:test(+Repository, +Style)
 %
 % Same as parser:test(+Repository), but uses a specified reporting style
-% 'single_verbose', 'parallel_verbose' or 'parallel_fast'.
+% 'single_verbose', 'parallel_verbose', or 'parallel_fast'.
 
-parser:test(Repository,Style) :-
+parser:test(Repository, Style) :-
   tester:test(Style,
               'Parsing',
               Repository://Entry,
-              (Repository:get_cache_file(Entry,File),reader:invoke(File,Codes)),
-              (parser:invoke(metadata,Repository://Entry,Codes,_))),!.
+              (Repository:get_cache_file(Entry, File), reader:invoke(File, Codes)),
+              (parser:invoke(metadata, Repository://Entry, Codes, _))), !.
