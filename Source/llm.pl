596f25d1f65e3a5c7bbf05fd6f28afbd16a94252
Pieter Van den Abeele <pvdabeel@mac.com>
2025-06-25 21:31:09 +0200

    Documentation, cleanup, ready for release

--- Source/llm.pl
+++ Source/llm.pl
@@ -22,7 +22,8 @@ Currently the following services are available:
 
 We also support real-time streaming to Ollama. (E.g. locally hosted)
 
-We support any model available on these services.
+We support any model available on these services. API url, key, model, max_tokens
+and temperature are configured in the config module. (cfr. config.pl).
 
 Standard features include:
 
@@ -37,6 +38,9 @@ Execution of commands:
 - LLMs can embed prolog code in specific tags and have that code executed locally
 - The code is executed in a sandbox, ensuring access to safe predicates only
 - The output is captured and provided back to the LLM for evaluation
+
+LLms can also send each other messages, by embedding the message in specific tags.
+Cfr. config.pl for the prompts that explain these capabilities to the LLM.
 
 LLM service specific code can be found in the files inside the 'Llm' subdirectory.
 */
@@ -55,78 +59,152 @@ LLM service specific code can be found in the files inside the 'Llm' subdirector
 :- use_module(library(sandbox)).
 
 
-% Streaming chat function for OpenAI-compatible services
-stream(Endpoint, APIKey, Model, Messages, Response) :-
-    config:llm_max_tokens(Max),
-    config:llm_temperature(Temperature),
-    set_stream(current_output, encoding(utf8)),
-    Payload = _{
-        model: Model,
-        messages: Messages,
-        stream: json_true,
-        max_tokens: Max,
-        temperature: Temperature
-    },
-    with_output_to(string(JsonString),
-                   json_write_dict(current_output, Payload, [true(json_true), width(0)])),
-    Headers = [method(post),
-               post(string(JsonString)),
-               authorization(bearer(APIKey)),
-               request_header('Content-Type'='application/json'),
-               request_header('Accept'='text/event-stream')],
+%! llm:stream(+Endpoint,+APIkey,+Model,+Message,-Response)
+%
+% Stream a message to an OpenAI-compatible LLM service.
+%
+%  - Endpoint: The URL through which the service can be reached
+%  - APIkey:   The API key that provides access to the service
+%  - Model:    The Large Language Model to be used
+%  - Message:  The actual message
+%
+% The response is written in real-time to the current_output
+% stream, and returned as a string through Response.
+%
+% Cfr config.pl for the different LLM configuration parameter.
 
-    catch(
-        (
-            http_open(Endpoint, In, Headers),
-            set_stream(In, encoding(utf8)),
+llm:stream(Endpoint, APIKey, Model, Messages, Response) :-
 
-            message:color(lightgray),
-            message:style(italic),
-            message:hl(Model),
+  % Retrieve some additional configuration parameters
+  config:llm_max_tokens(Max),
+  config:llm_temperature(Temperature),
 
-            process_stream(In, Contents),
-            close(In),
+  % This ensures emoji are displayed properly
+  set_stream(current_output,encoding(utf8)),
 
-	    nl,
-            message:hl,
-            message:color(normal),
-            message:style(normal),
+  % A Json payload is prepared
+  Payload = _{
+    model:       Model,
+    messages:    Messages,
+    stream:      json_true,
+    max_tokens:  Max,
+    temperature: Temperature
+  },
 
-            atomic_list_concat(Contents, ResponseContent),
-            % Find the last assistant message
-            reverse(Messages, Reversed),
-            find_first_assistant(Reversed, LastAssistantMessage),
-            % Update the last assistant message with full content
-            put_dict(content, LastAssistantMessage, ResponseContent, UpdatedAssistantMessage),
-            % Replace the last message in Messages with UpdatedAssistantMessage
-	    (  append(Prefix, [LastAssistantMessage|Rest], Messages)
-	    -> append(Prefix, [UpdatedAssistantMessage|Rest], NewMessages)
-            ;  append(Messages, [UpdatedAssistantMessage], NewMessages) ),
-            Response = _{contents: Contents, history: NewMessages}
+  % We convert the payload to a Json message, ensuring 'true' is not a string
+  % but an actual json true
+
+  with_output_to(string(JsonString),
+                 json_write_dict(current_output,
+                                 Payload,
+                                 [true(json_true),width(0)])),
+
+  % We prepare a http post call of the payload, ensuring authentication
+  % and setting streaming
+
+  Headers = [method(post),
+             post(string(JsonString)),
+             authorization(bearer(APIKey)),
+             request_header('Content-Type'='application/json'),
+             request_header('Accept'='text/event-stream')],
+
+  catch(( http_open(Endpoint,In,Headers),
+
+          % ensure we support emojo and complex characters
+          set_stream(In,encoding(utf8)),
+
+          % stream the message and nicely wrap its output
+          setup_call_cleanup((llm:begin_stream(Model)),
+                             (llm:process_stream(In,Contents)),
+                             (llm:end_stream(Model),
+                              close(In))),
+
+          % put the response in the message history
+          llm:prepare_history(Contents,Messages,NewMessages),
+
+          % set the response
+          Response = _{contents: Contents, history: NewMessages}
         ),
         Error,
         (
-            Response = _{error: Error, history: Messages}
+          Response = _{error: Error, history: Messages}
         )
-    ).
+   ).
 
-% Process the streaming response for OpenAI-compatible services
-process_stream(In, Contents) :-
-    process_stream(In, Contents, []).
 
-process_stream(In, Contents, Acc) :-
+%! llm:begin_stream(+Model)
+%
+% Execute activities prior to the LLM writing to the current_output stream
+
+llm:begin_stream(Model) :-
+  message:color(lightgray),
+  message:style(italic),
+  message:hl(Model).
+
+
+%! llm:end_stream(+Model)
+%
+% Execute activites post the LLM writing to the current_output stream
+
+llm:end_stream(_Model) :-
+  nl,
+  message:hl,
+  message:color(normal),
+  message:style(normal).
+
+
+%! llm:prepare_history(+Contents,+Messages,-NewMessages)
+%
+% Turn the different stream chuncks into a string representing the complete
+% sentence, add this sentence to the message list, with the right OpenAI
+% role.
+
+llm:prepare_history(Contents,Messages,NewMessages) :-
+  atomic_list_concat(Contents, ResponseContent),
+  reverse(Messages, Reversed),
+  find_first_assistant(Reversed, LastAssistantMessage),
+  put_dict(content, LastAssistantMessage, ResponseContent, UpdatedAssistantMessage),
+  (  append(Prefix, [LastAssistantMessage|Rest], Messages)
+     -> append(Prefix, [UpdatedAssistantMessage|Rest], NewMessages)
+     ;  append(Messages, [UpdatedAssistantMessage], NewMessages) ).
+
+
+%! llm:process_stream(+Stream,-Contents)
+%
+% OpenAI-compatible services send us data lines, each embedding a
+% part of the message. We need to process the different parts of
+% the message, until we receive a message saying the streaming is done.
+
+llm:process_stream(In, Contents) :-
+    llm:process_stream(In, Contents, []).
+
+
+%! llm:process_stream(+Stream,-Contents,-Acc)
+%
+% Helper predicate. Acc is used to accumulate the message contents.
+% We not write each part of the message to the current_output stream
+% as soon as we have received it. We don't wait for the full message
+% to be received. (LLM Streaming)
+
+llm:process_stream(In, Contents, Acc) :-
     read_line_to_string(In, Line),
     (  Line == end_of_file
     ->  reverse(Acc, Contents)
     ;  Line = ""
-    -> process_stream(In,Contents, Acc)
-    ;   process_line(Line, Content),
+    -> llm:process_stream(In,Contents, Acc)
+    ;   llm:process_line(Line, Content),
         (   Content == ""
-        ->  process_stream(In, Contents, Acc)
+        ->  llm:process_stream(In, Contents, Acc)
         ;   write(Content), flush_output,
-            process_stream(In, Contents, [Content|Acc])
+            llm:process_stream(In, Contents, [Content|Acc])
         )
     ).
+
+
+%! llm:process_line(+Line,-Content)
+%
+% Processes a line of content received from an OpenAI-compatible LLM
+% service. A line either contains data, or signals the streaming is done.
 
 process_line(Line, Content) :-
     (   sub_string(Line, 0, 6, _, "data: ")
@@ -134,43 +212,64 @@ process_line(Line, Content) :-
         (   Data = "[DONE]"
         ->  Content = ""
         ;   atom_json_dict(Data, Dict, []),
-            get_content_from_dict(Dict, Content)
+            llm:get_content_from_dict(Dict, Content)
         )
     ;   Content = ""
     ).
 
-get_content_from_dict(Dict, Content) :-
+
+%! llm:get_content_from_dict(+Dict,-Content)
+%
+% We receive a json dict structure in a data line from the OpenAI-compatible
+% LLM service. This predicate gets the content part out of that dict.
+
+llm:get_content_from_dict(Dict, Content) :-
     get_dict(choices, Dict, Choices),
     Choices = [Choice|_],
     get_dict(delta, Choice, Delta),
     get_dict(content, Delta, Content),
     !.
-get_content_from_dict(_,"") :- !.
+llm:get_content_from_dict(_,"") :- !.
 
 
-% Extract SWI-Prolog calls from response content
-%extract_swi_prolog_calls(ResponseContent, Matches) :-
-%    pcre:re_compile('<call:swi_prolog>(.*?)</call:swi_prolog>', Re, [dotall(true)]),
-%    pcre:re_foldl(llm:get_group,Re, ResponseContent, [], Matches, [optimize(true),capture_type(string)]).
 
-%get_group(Match, Acc, [Group|Acc]) :-
-%    get_dict(1, Match, Group).
+%! llm:extract_calls(+Contents,-Matches)
+%
+% In our communication flow, we inform the LLM in the first message that the LLM can
+% trigger specific capabilities by using XML tags in the message. We currently have two
+% possible messages:
+%
+%   - <call:swi_prolog> Some prolog code </call:swi_prolog> will execute the prolog
+%     code locally in a sandboxed context and send back the result to the LLM.
+%
+%   - <call:X> Some message </call:X> with X being either grok, gemini, claude, chatgpt
+%     or ollama, will send the message between the tags to the mentioned LLM and send
+%     back the result to the LLM. This allows LLMs to speak to each other.
+%
+% Matches is a list of Key-Value pairs. We use a compiled regular expression, which
+% keeps performance high, even with long messages.
 
-
-% Extract calls with dynamic keys from response content
-extract_calls(ResponseContent, Matches) :-
+llm:extract_calls(ResponseContent, Matches) :-
     pcre:re_compile('<call:([a-zA-Z0-9_]+)>(.*?)</call:\\1>', Re, [dotall(true)]),
     pcre:re_foldl(llm:extract_key_value, Re, ResponseContent, [], Matches, [optimize(true), capture_type(string)]).
 
+
+%! llm:extract_key_value(+Match,-Acc,-Result)
+%
 % Helper predicate to extract key-value pairs from regex matches
-extract_key_value(Match, Acc, [Key-Value|Acc]) :-
-    %pcre:re_matchsub(Re, Match, Groups, []),
+
+llm:extract_key_value(Match, Acc, [Key-Value|Acc]) :-
     get_dict(1, Match, Key),
     get_dict(2, Match, Value).
 
 
-% Sandboxed execution in a temporary module
-execute_llm_code(Src) :-
+%! llm:execute_llm_code(+Src)
+%
+% We set up a temporary,sandboxed, module in which we load the code passed by the llm.
+% This has to be called from within a with_output_to predicate to capture output for
+% both regular and error streams.
+
+llm:execute_llm_code(Src) :-
   config:llm_sandboxed_execution(Bool),
   open_chars_stream(Src,Stream),
   call_cleanup(catch((in_temporary_module(Module,
@@ -181,31 +280,38 @@ execute_llm_code(Src) :-
                close(Stream)).
 
 
-execute_and_get_output("grok"-Msg, Output) :-
+%! llm:execute_and_get_output(+Target-Msg,-Output)
+%
+% Based on the Target, handle the Message and return Output.
+
+% 1. Speaking to other LLM
+
+llm:execute_and_get_output("grok"-Msg, Output) :-
   grok(Msg,Output).
 
-execute_and_get_output("gemini"-Msg, Output) :-
+llm:execute_and_get_output("gemini"-Msg, Output) :-
   gemini(Msg,Output).
 
-execute_and_get_output("claude"-Msg, Output) :-
+llm:execute_and_get_output("claude"-Msg, Output) :-
   claude(Msg,Output).
 
-execute_and_get_output("chatgpt"-Msg, Output) :-
+llm:execute_and_get_output("chatgpt"-Msg, Output) :-
   chatgpt(Msg,Output).
 
-execute_and_get_output("ollama"-Msg, Output) :-
+llm:execute_and_get_output("ollama"-Msg, Output) :-
   ollama(Msg,Output).
 
 
-% Execute SWI-Prolog code safely and capture output
-execute_and_get_output("swi_prolog"-Code, Output) :-
+% 2. Execute SWI-Prolog code safely and capture output
+
+llm:execute_and_get_output("swi_prolog"-Code, Output) :-
     catch(
         (
             %read_term_from_atom(Code, Term, [variable_names(_)]),
             catch(
                 with_output_to(
                   string(OutputStr), %(call(Term) -> true ; format(string(OutputStr), 'Goal failed', []))),
-                  execute_llm_code(Code),
+                  llm:execute_llm_code(Code),
                   [capture([user_output,user_error]),color(true)]
                 ),
                 SafeError,
@@ -217,9 +323,14 @@ execute_and_get_output("swi_prolog"-Code, Output) :-
     ),
     Output = OutputStr.
 
-% Make function message
-make_function_message(Service,Content, Message) :-
-    %atomic_list_concat(['<command_output:swi_prolog>', Output, '</command_output:swi_prolog>'], Content),
+
+%! llm:make_function_message(+Service,+Content,-Message)
+%
+% Prepare a json message to be send back to the LLM.
+% Write the output to stream, wrapping it as computer output
+
+llm:make_function_message(Service,Content, Message) :-
+    % atomic_list_concat(['<command_output:swi_prolog>', Output, '</command_output:swi_prolog>'], Content),
     nl,
     message:hl('computer'),
     message:color(green),
@@ -232,59 +343,98 @@ make_function_message(Service,Content, Message) :-
      ;  Message = _{role: 'function', name: 'swi_prolog_exec', content: Content}).
 
 
-% Helper to find the first assistant message from the end
-find_first_assistant([Message|_Rest], Message) :-
-    get_dict(role, Message, 'assistant'), !.
-find_first_assistant([_|Rest], Message) :-
-    find_first_assistant(Rest, Message).
-find_first_assistant([], _{role: 'assistant', content: ''}).
+%! llm:find_first_assistant(+Messages,-Message)
+%
+% Helper to find the first assistant message in the message list.
 
-% Handle response with code execution
-handle_response(ServiceAPIKey, ServiceModel, ServiceEndpoint, Service:UpdateHistory, ResponseContent, History) :-
-  ( extract_calls(ResponseContent, Pairs),
+llm:find_first_assistant([Message|_Rest], Message) :-
+    get_dict(role, Message, 'assistant'), !.
+llm:find_first_assistant([_|Rest], Message) :-
+    llm:find_first_assistant(Rest, Message).
+llm:find_first_assistant([], _{role: 'assistant', content: ''}).
+
+
+
+%! llm:handle_response(+ApiKey,+Model,+Endpoint,+HistoryUpdater,+Response,+History)
+%
+% Called with the response of the LLM, handles execution and message passing to other LLM
+
+llm:handle_response(ServiceAPIKey, ServiceModel, ServiceEndpoint, Service:UpdateHistory, ResponseContent, History) :-
+  ( llm:extract_calls(ResponseContent, Pairs),
     Pairs \= []
-    ->  maplist(execute_and_get_output, Pairs, Outputs),
-        maplist(make_function_message(Service), Outputs, FunctionMessages),
+
+    ->  % something to process, execute and get ouput:
+
+        maplist(llm:execute_and_get_output, Pairs, Outputs),
+
+        % prepare the automated response to the llm
+
+        maplist(llm:make_function_message(Service), Outputs, FunctionMessages),
         append(History, FunctionMessages, UpdatedHistory),
+
+        % send back the automated response to the llm
+
         stream(ServiceEndpoint, ServiceAPIKey, ServiceModel, UpdatedHistory, NewResponse),
+
+        % handle the new response from the
         (
             NewResponse = _{contents: NewContents, history: NewerHistory}
-        ->
-            atomic_list_concat(NewContents, NewResponseContent),
-            handle_response(ServiceAPIKey, ServiceModel, ServiceEndpoint, Service:UpdateHistory, NewResponseContent, NewerHistory)
-        ;   NewResponse = _{error: NewError, history: _}
-        ->  write('Error: '), write(NewError), nl
-        )
-    ;   %write(ResponseContent), nl,
-        call(Service:UpdateHistory, History)
-    ).
+            ->
+            % keep handling responses
 
-% Input handling
-get_input(Msg) :-
+            atomic_list_concat(NewContents, NewResponseContent),
+            llm:handle_response(ServiceAPIKey, ServiceModel, ServiceEndpoint, Service:UpdateHistory, NewResponseContent, NewerHistory)
+
+            ;   NewResponse = _{error: NewError, history: _}
+                ->  write('Error: '), write(NewError), nl
+        )
+    ;
+       % update the history
+       call(Service:UpdateHistory, History)
+   ).
+
+
+%! llm:get_input(Msg)
+%
+% Launches the configured editor on a temporary file, and returns file contents on editor
+% exit. Temporary file is then deleted.
+%
+% Allows to use vim / your favorite editor to send a message to the LLM
+
+llm:get_input(Msg) :-
     tmp_file(chat_input, File),
     edit(File),
     read_file_to_string(File, Msg, []),
     delete_file(File).
 
 
-% Informs the LLM about our capabilities
-prompt(Prompt) :-
-  findall(Capability, config:llm_capability(_, Capability), Cs),
+%! llm:prompt(Prompt)
+%
+% Put together a prompt for the LLM based on capabilities defined in config.pl
+
+llm:prompt(Prompt) :-
+  findall(Capability,
+          config:llm_capability(_, Capability),
+          Cs),
   atomic_list_concat(Cs, ' ', Capabilities),
-  string_concat(Capabilities," This was a prompt message, no need to acknowledge or respond to the previous sentences. Everything behind this final sentence is not a prompt and can be reacted to and acknowledged as regular input.",Prompt).
+  PromptEnding = " This was a prompt message, no need to acknowledge or respond to the previous sentences.
+                   Everything behind this final sentence is not a prompt and can be reacted to and acknowledged as regular input.",
+  normalize_space(string(Ending),PromptEnding),
+  string_concat(Capabilities,Ending,Prompt).
 
 
-% Adds prompt tp to the first message to the LLM
+%! llm:prepare_message(+History,+Role,+Input,-Messages)
+%
+% Adds prompt in the first message to the LLM
 
-prepare_message(History,Role,Input,Messages) :-
-  add_prompt(History,Input,Msg),
+llm:prepare_message(History,Role,Input,Messages) :-
+  llm:add_prompt(History,Input,Msg),
   UserMessage = _{role: Role, content: Msg},
   append(History,[UserMessage],Messages).
 
-add_prompt([],Msg,NewMsg) :-
+llm:add_prompt([],Msg,NewMsg) :-
   !,
-  prompt(P),
+  llm:prompt(P),
   string_concat(P,Msg,NewMsg).
 
-add_prompt(_,Msg,Msg) :- !.
-
+llm:add_prompt(_,Msg,Msg) :- !.
